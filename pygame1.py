04c6ad9d0b6d9406e5f8eb00e32f745ad6383d10
Carl Shurtliff <cphilshurtliff@gmail.com>
2019-04-28 13:36:06 -0600

    saving the remote changes

--- pygame1.py
+++ pygame1.py
@@ -4,56 +4,71 @@ import time
 
 pygame.init()
 
+
+
+
+
+pink = (255, 100, 100)
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
-
+green = (0, 255, 0)
 position = [400, 300]
 userInput = "no input"
-
-
-
+bullet_pos = [position[0] + 10, position[1] - 30, 5, 10]
+bullet_bool = False
+bullet_time = 80
+enemy_pos = [400, 100]
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('game_of_the_bloxx')
 
 
-gameExit = False 
+gameExit = False
 
 while not gameExit:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
 	gameDisplay.fill(black)
-	pygame.draw.rect(gameDisplay, red, [position[0], position[1], 10, 10])
-
+	pygame.draw.rect(gameDisplay, red, [position[0], position[1], 10, 10])	
+	pygame.draw.rect(gameDisplay, white, [position[0] + 20, position[1], 10, 10])
+	pygame.draw.rect(gameDisplay, green, [position[0] + 10, position[1] - 30, 10, 20])
+        pygame.draw.rect(gameDisplay, green, [enemy_pos[0], enemy_pos[1], 30, 30])
+        pygame.draw.rect(gameDisplay, pink, [bullet_pos[0], bullet_pos[1], bullet_pos[2], bullet_pos[3]]) 
 	time.sleep(.01)
 	pressed = pygame.key.get_pressed()
+	if bullet_bool:
+		bullet_pos[1] = bullet_pos[1] - 10
+		bullet_time = bullet_time - 1
+		if bullet_time <= 0:
+			bullet_bool = False
+			bullet_time = 80
+
+	if pressed[pygame.K_SPACE] and bullet_bool == False:
+		bullet_bool = True
+		bullet_pos[1] = position[1] - 20
+		bullet_pos[0] = position[0] + 13
+
+
 	if pressed[pygame.K_w]:
 		position[1] = position[1] - 10
+        if position[1] <= 30:
+            position[1] = 30
 	if pressed[pygame.K_s]:
 		position[1] = position[1] + 10
+        if position[1] >= 590:
+            position[1] = 590
 	if pressed[pygame.K_a]:
 		position[0] = position[0] - 10
+	if position[0] <= 0:
+		position[0] = 0
 	if pressed[pygame.K_d]:
 		position[0] = position[0] + 10
-
-
-
-
-
-
-
-
-
-#	if event.type == pygame.KEYDOWN:
-#		if event.key == pygame.K_d:
-#			position[0] = position[0] + 10
-#		if event.key == pygame.K_a:
-#			position[0] = position[0] - 10
-#		if event.key == pygame.K_w:
-#			position[1] = position[1] - 10
-#		if event.key == pygame.K_s:
-#			position[1] = position[1] + 10
+	if position[0] >= 770:
+		position[0] = 770
+        
+        bullet_pos[0]
+        
 
 	pygame.display.update()
 
